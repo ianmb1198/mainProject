5900937e60278ce75448d74cc9dadfc3a25b3e35
Ian <Ian@DESKTOP-2B8CSG7>
2019-11-08 13:15:11 -0600

    fd

--- MicrosPOS/src/Appetizer.java
+++ MicrosPOS/src/Appetizer.java
@@ -1,40 +1,56 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-public class Appetizer extends Menu
+public class Appetizer
 {
-	private double price;
-	private String item;
 	
-	public Appetizer(String type, String item, double price)
+	public Appetizer()
 	{
-		super(type);
-		this.item = item;
-		this.price = price;
+		appReader();
 	}
 	
-	public void setPrice(double price)
+	public void appReader()
 	{
-		this.price = price;
-	}
-	public void setItem(String item)
-	{
-		this.item = item;
-	}
-	
-	public double getPrice()
-	{
-		return price;
-	}
-	public String getItem()
-	{
-		return item;
-	}
-	
-	@Override
-	public String toString()
-	{
-		String appInfo = item + "\t$" + price;
-		return appInfo;
-	}
-	
-	
+		Scanner cin = new Scanner(System.in);
+		Scanner fileIn = null;
+		String[] item = new String[8];
+		double[] price = new double[8];
+		
+		try
+		{
+			fileIn = new Scanner(new FileInputStream("Appetizer.txt"));
+		} catch (FileNotFoundException e)
+		{
+			System.out.println("\nFile Not Found...\n");
+		}
+		
+		int index = 0;
+		String appMenu = null;
+		while (fileIn.hasNext())
+		{
+			appMenu = fileIn.nextLine();
+			String[] strSplit = appMenu.split(",");
+			item[index] = strSplit[0];
+			price[index] = Double.parseDouble(strSplit[1]);
+			index++;
+		}
+		
+		System.out.println("***Appetizers***");
+		for (int i = 0; i < 8; i++)
+		{
+			System.out.println((i + 1) + ". " + item[i] + " $" + price[i]);
+		}
+		System.out.println("****************");
+		System.out.print("--> ");
+		int appChoice = cin.nextInt();
+		cin.nextLine();
+		
+		String itemOfChoice = item[appChoice - 1];
+		double priceOfItem = price[appChoice - 1];
+		
+		
+		//TabWriter(item, price, appChoice);
+		
+	}	
 }
